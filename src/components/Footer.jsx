bf91b84c8f52fe03caaf5e9f8e1ab751ac6beba7
jadhav-rushi-dev <rhushikesh.jadhav26@gmail.com>
2024-10-31 21:19:12 +0530

    Initial Commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,64 @@
+import { resourcesLinks, platformLinks, communityLinks } from "../constants";
+
+const Footer = () => {
+  return (
+    <footer className="mt-20 border-t py-10 border-neutral-700">
+      <div className="grid grid-cols-2 lg:grid-cols-3 gap-4">
+        <div>
+          <h3 className="text-md font-semibold mb-4">Resourses</h3>
+            <ul>
+              {resourcesLinks.map((link, index) => (
+                <li key={index}>
+                  <a
+                    href={link.href}
+                    className="text-neutral-300 hover:text-white"
+                  >
+                    {link.text}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          
+        </div>
+
+        <div>
+          <h3 className="text-md font-semibold mb-4">Platform</h3>
+            <ul>
+              {platformLinks.map((platform, index) => (
+                <li key={index}>
+                  <a
+                    href={platform.href}
+                    className="text-neutral-300 hover:text-white"
+                  >
+                    {platform.text}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          
+        </div>
+
+
+        <div>
+          <h3 className="text-md font-semibold mb-4">Community</h3>
+            <ul>
+              {communityLinks.map((community, index) => (
+                <li key={index}>
+                  <a
+                    href={community.href}
+                    className="text-neutral-300 hover:text-white"
+                  >
+                    {community.text}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          
+        </div>
+
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
